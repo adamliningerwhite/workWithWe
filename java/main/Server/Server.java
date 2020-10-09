b868da3bc01f57053485f058906e7d39c7c9663c
Adam L-Dub <adamliningerwhite@gmail.com>
2020-10-09 16:13:16 -0700

    Basic main server

--- java/main/Server/Server.java
+++ java/main/Server/Server.java
@@ -1,70 +1,80 @@
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.*; 
+import java.text.*; 
+import java.util.*; 
+import java.net.*; 
 
 public class Server {
-	
-    public Server(String listenPortStr) throws Exception {
 
-        //notify the identity of the server to the user
-        System.out.println("This is the Server");
-        
-        //attempt to create a server with the given port number
-        int listenPort = Integer.parseInt(listenPortStr);
-        try {
-            System.out.println("Connecting to port "+listenPort+"...");
-            ServerSocket bobServer = new ServerSocket(listenPort);
-            System.out.println("Server started at port "+listenPort);
-            
-            //accept the client(a.k.a. Alice)
-            Socket clientSocket = bobServer.accept();
-            System.out.println("Client connected");
-            DataInputStream streamIn = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
-                
-            boolean finished = false;
-                
-            //read input from Alice
-            while(!finished) {
-                try {
-                    String incomingMsg = streamIn.readUTF();
-                    System.out.println("Recieved msg: " + incomingMsg);
-                    
-                    finished = incomingMsg.equals("done");
-                }
-                catch(IOException ioe) {
-                    //disconnect if there is an error reading the input
-                    finished = true;
-                }
+    private static final int USER_LISTEN_PORT = 4232;
+    private static final int MESSAGE_LISTEN_PORT = 4771;
+
+    private static List<String> onlineUsers = new ArrayList<String>();
+	
+    public Server() throws Exception {
+
+        /* Start workWithMe server */
+        System.out.println("Started WorkWithMe Server");
+        ServerSocket mainServer = new ServerSocket(USER_LISTEN_PORT);
+
+        /* infinite loop to accept user connections */
+        while (true) {
+            Socket s = null;
+            String username;
+
+            try {
+                /* Accept incoming connections */
+                s = mainServer.accept();
+                System.out.println("New user connected");
+
+                /* obtaining input and out streams */
+                DataInputStream dis = new DataInputStream(s.getInputStream()); 
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+                /* 1st message: new user object sends its username */ 
+                username = dis.readUTF();
+
+                /* create and start a new user thread */
+                UserHandler t = new UserHandler(s, dis, dos, username, onlineUsers);
+
+                /* add new user to our list of people online*/
+                onlineUsers.add(username);
+
+                /* 1st response: notify user has been logged in */
+                dos.writeUTF(packageMessage(username + " successfully logged in"));
+
+            } catch (Exception e) {
+                s.close();
+                e.printStackTrace();
+
+                break; // TODO: don't think we want to break here, but otherwise next block is unreachable
             }
-            
-            //clean up the connections before closing
-            bobServer.close();
-            streamIn.close();
-            System.out.println("Server closed");
         } 
-        catch (IOException e) {
-            //print error if the server fails to create itself
-            System.out.println("Error in creating the server");
-            System.out.println(e);
+
+        // NOT SURE WHEN/HOW TO HANDLE CLOSING MAIN SERVER
+        try {
+            mainServer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-	
+
+	private String packageMessage(String message) throws Exception {
+        StringBuilder acc = new StringBuilder();
+        acc.append(message);
+        return acc.toString();
+    }
+   
     /**
-     * args[0] ; port that Alice will connect to
-     * args[1] ; program configuration
+     * main method 
      */
     public static void main(String[] args) {
         //check for correct # of parameters
-        if (args.length != 1) {
+        if (args.length > 0) {
             System.out.println("Incorrect number of parameters");
             return;
         }
-        
-        //Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-        
-        //create Bob
         try {
-            Server server = new Server(args[0]);
+            Server server = new Server();
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
