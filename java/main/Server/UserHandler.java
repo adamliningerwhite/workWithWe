357effdc0179b98b99c611cbd38372d8a33b096a
Renae Tamura <renae.tamura@pomona.edu>
2020-11-09 11:27:18 -0800

    key transport taken out

--- java/main/Server/UserHandler.java
+++ java/main/Server/UserHandler.java
@@ -36,17 +36,18 @@ public class UserHandler extends Thread {
         
         while (true)  { 
             try { 
-            	String msg;
-            	String input = dis.readUTF();
-            	String[] lines = input.split("[\\r\\n]");
-            	if(lines.length > 1) {
-            		String noMac = lines[0]; 
-                    msg = lines[1];
-                    msg = encHelper.getDecodedMessage(msg, noMac);
-                    System.out.println(msg);
-            	} else {
-            		msg = input;
-            	}
+//            	String msg;
+            	String msg = dis.readUTF();
+//            	String input = dis.readUTF();
+//            	String[] lines = input.split("[\\r\\n]");
+//            	if(lines.length > 1) {
+//            		String noMac = lines[0]; 
+//                    msg = lines[1];
+//                    msg = encHelper.getDecodedMessage(msg, noMac);
+//                    System.out.println(msg);
+//            	} else {
+//            		msg = input;
+//            	}
                 
                 if(msg.equals("Logoff")) {  
                     System.out.println(username + " is logging off...");
