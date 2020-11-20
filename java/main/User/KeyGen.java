6f4335c89d995bddebbb9e1e98c592ee7e8e27b9
jackbernstein <john.bernstein@pomona.edu>
2020-11-19 16:00:20 -0800

    user interface for showing info

--- java/main/User/KeyGen.java
+++ java/main/User/KeyGen.java
@@ -23,22 +23,22 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class KeyGen {
-	
+
 	public static Base64.Encoder encoder = Base64.getEncoder();
 	public static Base64.Decoder decoder = Base64.getDecoder();
-	
+
 	private SecretKey macKey;
 	private SecretKey encodingKey;
 	private SecretKey sessionKey;
-	
+
 	private RSAPrivateKey userKey;
 	private RSAPublicKey serverKey;
-	
+
 	private String username;
 	private String password;
 	private String userLoginType;
 	private String keyTransportMsg;
-	
+
 	public KeyGen(String username, String password, String userLoginType, RSAPublicKey serverKey) {
 	//public KeyGen(String username) {
 		this.username = username;
@@ -49,39 +49,39 @@ public class KeyGen {
 			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
 			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
 			keyGen.initialize(1024, random);
-			
+
 			KeyPair userPair = keyGen.generateKeyPair();
 			saveKeyPair(username, userPair);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	
+
 		this.userKey = readPrivateKeyFromFile("UserKeys/" + username + "private.key");
 		//System.out.println(getKeyTransportMsg());
-		
+
 	}
-	
+
 	/* methods to save key pairs and create and get the key transport message */
 	private void saveKeyPair(String name, KeyPair keyPair) throws IOException {
 		PrivateKey privateKey = keyPair.getPrivate();
 		PublicKey publicKey = keyPair.getPublic();
-		
+
 		X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(publicKey.getEncoded());
 		FileOutputStream fos = new FileOutputStream("UserKeys/" + name + "public.key");
 		fos.write(x509KeySpec.getEncoded());
 		fos.close();
-		
+
 		PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(privateKey.getEncoded());
 		fos = new FileOutputStream("UserKeys/" + name + "private.key");
 		fos.write(pkcs8KeySpec.getEncoded());
 		fos.close();
 	}
-	
+
 	public String getKeyTransportMsg() {
 		keyTransportMsg = keyTransport();
 		return keyTransportMsg;
 	}
-	
+
 	private String keyTransport() {
     	String message = null;
     	try {
@@ -91,31 +91,31 @@ public class KeyGen {
 			SecureRandom random = new SecureRandom();
 			keyGen.init(random);
 			sessionKey = keyGen.generateKey();
-			
+
 			// create string then byte array of message to be encoded
 			String encodedSessionKey = encoder.encodeToString(sessionKey.getEncoded());
-	        
+
 			byte[] s = decoder.decode(hashFunction(encodedSessionKey, "enc"));
 			byte[] encKey = Arrays.copyOfRange(s, 0,32);
 			SecretKeySpec macSks = new SecretKeySpec(decoder.decode(hashFunction(encodedSessionKey, "mac")), "AES");
 			SecretKeySpec encSks = new SecretKeySpec(encKey, "AES");
 			macKey = macSks;
 			encodingKey = encSks;
-			
+
 			String msg = userLoginType + "," + username + "," + password + "\n" + encodedSessionKey;
 			byte[] toEncode = msg.getBytes();
-			
+
 			// create cipher and encode message with Alice identifier and session key
 			Cipher cipher = Cipher.getInstance("RSA");
 			cipher.init(Cipher.ENCRYPT_MODE, serverKey, random);
 			byte[] cipherText = cipher.doFinal(toEncode);
-			
+
 			// create string then byte array of message to be signed
 			String cipherString = encoder.encodeToString(cipherText);
 			String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date(System.currentTimeMillis()));
 			String msg2 = "Server" + "\n" + timeStamp + "\n" + cipherString;
 			byte[] toSign = msg2.getBytes();
-			
+
 			// create signature with SHA256 and RSA then sign message
 			Signature sign = Signature.getInstance("SHA256withRSA");
 			sign.initSign(userKey);
@@ -123,34 +123,34 @@ public class KeyGen {
 			String signature = encoder.encodeToString(sign.sign());
 
 			toSign = (msg2 + "\r\n" + signature).getBytes();
-			
-			// update message to new string 
+
+			// update message to new string
 			message = new String(toSign);
 
 		} catch (Exception e) {
 			System.out.println("Error: key transport msg not created");
 			e.printStackTrace();
-		} 
-    	
+		}
+
     	return message;
     }
-	
+
 //	public void setSessionKey(SecretKey key) {
 //		sessionKey = key;
 //	}
-	
+
 //	private SecretKey getMacKey() {
 //		try {
 //			String encodedSessionKey = encoder.encodeToString(sessionKey.getEncoded());
 //			SecretKeySpec macSks = new SecretKeySpec(decoder.decode(hashFunction(encodedSessionKey, "mac")), "AES");
-//			macKey = macSks;	
+//			macKey = macSks;
 //		} catch(NoSuchAlgorithmException e) {
 //			e.printStackTrace();
 //		}
-//		
-//		return macKey;	
+//
+//		return macKey;
 //	}
-	
+
 //	private SecretKey getEncodeKey() {
 //		try {
 //			String encodedSessionKey = encoder.encodeToString(sessionKey.getEncoded());
@@ -161,10 +161,10 @@ public class KeyGen {
 //		} catch(NoSuchAlgorithmException e) {
 //			e.printStackTrace();
 //		}
-//		
+//
 //		return encodingKey;
 //	}
-	
+
 	/* methods to encrypt messages from user with and without mac */
 	public String createEncoded(String msg) {
 		try {
@@ -174,10 +174,10 @@ public class KeyGen {
 			System.out.println("Error: package could not be encoded");
 			e.printStackTrace();
 		}
-		
+
 		return msg;
 	}
-	
+
 	public String createEncodedMessage(String msg) {
 		try {
 			msg = packageMessage(msg);
@@ -187,25 +187,25 @@ public class KeyGen {
 			System.out.println("Error: package could not be encoded with MAC");
 			e.printStackTrace();
 		}
-		
+
 		return msg;
 	}
-	
+
 	private String encrypt(String message) throws Exception {
-		
+
 		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	    IvParameterSpec ivspec = new IvParameterSpec(iv);
 		cipher.init(Cipher.ENCRYPT_MODE, encodingKey, ivspec);
 		byte[] encrypted = cipher.doFinal(message.getBytes());
 		String encodedString = encoder.encodeToString(encrypted);
-		
+
 		return encodedString;
 	}
-    
+
     public String createMac(String message) throws Exception {
     	String encrypted = "";
-    	
+
 		Mac mac = Mac.getInstance("HmacSHA256");
 		mac.init(macKey);
 		byte[] stringBytes = message.getBytes();
@@ -214,19 +214,18 @@ public class KeyGen {
 
     	return encrypted;
     }
-    
+
     private String packageMessage(String message) throws Exception {
 		StringBuilder acc = new StringBuilder();
 		acc.append(message);
 
 		return acc.toString();
 	}
-	
-    /* methods to decrypt message from server */ 
+
+    /* methods to decrypt message from server */
     public String getDecodedMessage(String msg, String noMac) {
 		try {
 			if(!checkMac(msg, noMac)) {
-				System.out.println("Mac not verified");
 				//return "break";
 			}
 			msg = decrypt(noMac);
@@ -234,10 +233,10 @@ public class KeyGen {
 			System.out.println("Message not decoded");
 			e.printStackTrace();
 		}
-		
+
 		return msg;
 	}
-	
+
 	private String decrypt(String cipherText) throws IOException, GeneralSecurityException {
 		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -253,7 +252,7 @@ public class KeyGen {
 			byte[] stringBytes = msg.getBytes();
 			byte[] macBytes = mac.doFinal(stringBytes);
 			String newMacStr = new String(macBytes);
-			
+
 			//System.out.println("new mac str: " + newMacStr);
 			//System.out.println("mac str: " + macStr);
 
@@ -265,30 +264,30 @@ public class KeyGen {
 
 		return false;
 	}
-    
+
 	/* general methods such as hash function and reading keys from file */
 	private String hashFunction(String input, String concat) throws NoSuchAlgorithmException {
-		 // getInstance() method is called with algorithm SHA-512 
-       MessageDigest md = MessageDigest.getInstance("SHA-512"); 
+		 // getInstance() method is called with algorithm SHA-512
+       MessageDigest md = MessageDigest.getInstance("SHA-512");
 
-       // digest() method is called 
-       // to calculate message digest of the input string 
-       // returned as array of byte 
-       byte[] messageDigest = md.digest((input+concat).getBytes()); 
+       // digest() method is called
+       // to calculate message digest of the input string
+       // returned as array of byte
+       byte[] messageDigest = md.digest((input+concat).getBytes());
 
-       // Convert byte array into signum representation 
-       BigInteger no = new BigInteger(1, messageDigest); 
+       // Convert byte array into signum representation
+       BigInteger no = new BigInteger(1, messageDigest);
 
-       // Convert message digest into hex value 
-       String hashtext = no.toString(16); 
+       // Convert message digest into hex value
+       String hashtext = no.toString(16);
 
-       // Add preceding 0s to make it 32 bit 
-       while (hashtext.length() < 32) { 
-           hashtext = "0" + hashtext; 
-       } 
+       // Add preceding 0s to make it 32 bit
+       while (hashtext.length() < 32) {
+           hashtext = "0" + hashtext;
+       }
        return hashtext;
    }
-	
+
 	private static RSAPrivateKey readPrivateKeyFromFile(String filePath) {
 		RSAPrivateKey key = null;
 
@@ -303,7 +302,7 @@ public class KeyGen {
 		}
 		return key;
 	}
-	
+
 //	private static RSAPublicKey readPublicKeyFromFile(String filePath) {
 //		RSAPublicKey key = null;
 //		File publicKey = new File(filePath);
@@ -321,12 +320,12 @@ public class KeyGen {
 //		}
 //		return key;
 //	}
-	
+
 	public static void main(String[] args) {
 		//KeyGen serverGen = new KeyGen("server");
 		//RSAPublicKey publicServer = readPublicKeyFromFile("../Server/ServerKeys/serverpublic.key");
 		//KeyGen keyGen = new KeyGen("jack", publicServer);
-		
+
 		//RSAPublicKey publicServer = readPublicKeyFromFile("../Server/ServerKeys/serverpublic.key");
 		//SecretKey encoded = getEncodeKey();
 		//SecretKey mac = getMacKey();
--- java/main/User/ServerHandler.java
+++ java/main/User/ServerHandler.java
@@ -12,6 +12,13 @@ public class ServerHandler extends Thread {
 	private KeyGen keyGen;
 	private boolean running = true;
 
+	private HashSet<String> friends = new HashSet<String>();
+	private HashSet<String> requests = new HashSet<String>();
+	private HashSet<String> accepts = new HashSet<String>();
+	private HashSet<String> rejects = new HashSet<String>();
+
+	boolean different = true;
+
 
 	public ServerHandler( DataOutputStream streamOut, DataInputStream streamIn, KeyGen keyGen) {
 		this.streamOut = streamOut;
@@ -41,20 +48,78 @@ public class ServerHandler extends Thread {
         res = keyGen.getDecodedMessage(res, noMac);
       	} else {
       		res = input;
-      	}	
-				System.out.println(res);
-				for(int i = 0; i < 1000; i++) {
-					if(running) {
-						Thread.sleep(10);
-					} else {
-						break;
-					}
+      	}
+				parser(res);
+				if(different) {
+					results();
 				}
+				different = false;
+				Thread.sleep(10);
 			}
 		} catch (Exception e) {
 			System.out.println("Error: heartbeat message not sent");
 			e.printStackTrace();
 		}
+	}
+
+	private void parser(String input) {
+		String[] lines = input.split("\n");
+		for(String line : lines){
+			String[] values = line.split(",");
+			if(values.length > 0) {
+				switch(values[0]) {
+					case "1":
+						friends = compareLists(values, friends);
+						break;
+					case "2":
+						requests = compareLists(values, requests);
+						break;
+					case "3":
+						break;
+					case "4":
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+
+	private HashSet<String> compareLists(String[] values, HashSet<String> list) {
+		HashSet<String> freshList = new HashSet<String>();
+		for(int i = 1; i < values.length; i++)
+			freshList.add(values[i]);
+		if(freshList.size() != list.size()) {
+			different = true;
+			return freshList;
+		} else {
+			for(String friend : freshList) {
+				if(!list.contains(friend)) {
+					different = true;
+					return freshList;
+				}
+			}
+		}
+		return list;
+	}
+
+	private void results() {
+		String friendList = "Friend list: ";
+		for(String friend : friends)
+			friendList += friend + ", ";
+		if(friends.size() == 0)
+			friendList = "You have no friends ya loser";
+		else
+			friendList = friendList.substring(0, friendList.length() - 2);
+		System.out.println(friendList);
+		String pendingList = "Pending friend requests: ";
+		for(String friend : requests)
+			pendingList += friend + ", ";
+		if(requests.size() == 0)
+			pendingList = "No pending friend requests";
+		else
+			pendingList = pendingList.substring(0, pendingList.length() - 2);
+		System.out.println(pendingList);
 	}
 
 	public void end() {
